423f32f8f046c14313cabd9bf3ce292e1f28b5e7
Khanh Toan Bui <khanhtoanbui@Toans-MacBook-Pro-3.local>
2018-11-27 14:32:31 -0800

    Wifi fixed

--- CARApp/app/src/main/java/com/car/carsquad/carapp/SplashScreenActivity.java
+++ CARApp/app/src/main/java/com/car/carsquad/carapp/SplashScreenActivity.java
@@ -1,5 +1,6 @@
 package com.car.carsquad.carapp;
 
+import android.app.Activity;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.DialogInterface;
@@ -33,59 +34,79 @@ public class SplashScreenActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         Objects.requireNonNull(actionBar).hide();
 
-        //check network status
-        boolean connected = false;
-        ConnectivityManager connectivityManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
-        if(connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState() == NetworkInfo.State.CONNECTED ||
-                connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState() == NetworkInfo.State.CONNECTED) {
-            //we are connected to a network
-            connected = true;
-        }
-        else
-            connected = false;
-
-
         if(haveNetworkConnection()) {
-            //Firebase Authentication Objects
-            FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
-            FirebaseUser user = firebaseAuth.getCurrentUser();
-
-            //if User is already logged in, skip this activity
-            if (user != null && user.isEmailVerified()) {
-                DatabaseReference databaseUser =
-                        FirebaseDatabase.getInstance().getReference("users");
-                final String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
-
-                databaseUser.child(userId).child("currentMode").addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                        String currentMode;
-                        currentMode = dataSnapshot.getValue(String.class);
-                        if (Objects.equals(currentMode, "driver")) {
-                            startActivity(new Intent(getApplicationContext(), DriverActivity.class));
-                        } else if (Objects.equals(currentMode, "rider")) {
-                            startActivity(new Intent(SplashScreenActivity.this, RiderActivity.class));
-                        } else {
-                            startActivity(new Intent(getApplicationContext(), MainActivity.class));
-                        }
-                    }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError databaseError) {
-                    }
-                });
-            } else {
-                Intent homeIntent = new Intent(Intent.ACTION_MAIN);
-                homeIntent.addCategory( Intent.CATEGORY_HOME );
-                homeIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-                startActivity(homeIntent);
-
-                startActivity(new Intent(getApplicationContext(), MainActivity.class));
-            }
+            proceedActivity();
         }
         //if not connected to internet
         else {
             showDialog();
+        }
+    }
+
+
+
+    private void showDialog()
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("You are OFFLINE")
+                .setCancelable(false)
+                .setPositiveButton("Connect to WIFI", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        //startActivity(new Intent(Settings.ACTION_WIFI_SETTINGS));
+                        //finish();
+                        //startActivityForResult(new Intent(android.provider.Settings.ACTION_SETTINGS), 0);
+                        finish();
+                        startActivityForResult(new Intent(android.provider.Settings.ACTION_WIRELESS_SETTINGS), 0);
+                        if(haveNetworkConnection()){
+                            proceedActivity();
+                        }
+                    }
+                })
+                .setNegativeButton("Quit", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        finish();
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    private void proceedActivity() {
+        //Firebase Authentication Objects
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+
+        //if User is already logged in, skip this activity
+        if (user != null && user.isEmailVerified()) {
+            DatabaseReference databaseUser =
+                    FirebaseDatabase.getInstance().getReference("users");
+            final String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+            databaseUser.child(userId).child("currentMode").addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    String currentMode;
+                    currentMode = dataSnapshot.getValue(String.class);
+                    if (Objects.equals(currentMode, "driver")) {
+                        startActivity(new Intent(getApplicationContext(), DriverActivity.class));
+                    } else if (Objects.equals(currentMode, "rider")) {
+                        startActivity(new Intent(SplashScreenActivity.this, RiderActivity.class));
+                    } else {
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+                }
+            });
+        } else {
+            Intent homeIntent = new Intent(Intent.ACTION_MAIN);
+            homeIntent.addCategory( Intent.CATEGORY_HOME );
+            homeIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(homeIntent);
+
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
         }
     }
 
@@ -106,25 +127,4 @@ public class SplashScreenActivity extends AppCompatActivity {
         return haveConnectedWifi || haveConnectedMobile;
     }
 
-    private void showDialog()
-    {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setMessage("Connect to wifi or quit")
-                .setCancelable(false)
-                .setPositiveButton("Connect to WIFI", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        //startActivity(new Intent(Settings.ACTION_WIFI_SETTINGS));
-                        finish();
-                        startActivityForResult(new Intent(android.provider.Settings.ACTION_SETTINGS), 0);
-                        //finish();
-                    }
-                })
-                .setNegativeButton("Quit", new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        finish();
-                    }
-                });
-        AlertDialog alert = builder.create();
-        alert.show();
-    }
 }
