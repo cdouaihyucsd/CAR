1b1344cee17585b6862054a6fec1731091bbb9cf
joshydotpoo <j.freilichd@gmail.com>
2018-12-02 13:26:50 -0800

    Adding FCMReceiver.java and ServerUploader.java.

--- CARApp/app/src/main/java/com/car/carsquad/carapp/MainActivity.java
+++ CARApp/app/src/main/java/com/car/carsquad/carapp/MainActivity.java
@@ -1,23 +1,19 @@
 package com.car.carsquad.carapp;
 
 import android.content.Intent;
-import android.support.annotation.NonNull;
+import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -86,4 +82,6 @@ public class MainActivity extends AppCompatActivity {
     public void onBackPressed() {
         moveTaskToBack(true);
     }
+
+
 }
