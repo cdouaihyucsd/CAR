8eb4ac7557e5c9d905cfccd76a031d6a8262e888
Khanh Toan Bui <khanhtoanbui@Toans-MacBook-Pro-3.local>
2018-12-03 21:17:26 -0800

    fixed

--- CARApp/app/src/main/java/com/car/carsquad/carapp/ChatRoomActivity.java
+++ CARApp/app/src/main/java/com/car/carsquad/carapp/ChatRoomActivity.java
@@ -3,7 +3,7 @@ package com.car.carsquad.carapp;
 
 import android.bluetooth.BluetoothClass;
 import android.os.Bundle;
-import android.os.Message;
+
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.NotificationCompat;
