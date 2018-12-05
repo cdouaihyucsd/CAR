feb64ac611fee38f147c429d4647daf13df84131
Khanh Toan Bui <khanhtoanbui@Toans-MacBook-Pro-3.local>
2018-12-04 19:04:35 -0800

    NEW FIX

--- CARApp/app/src/main/java/com/car/carsquad/carapp/DriverFinishedRideActivity.java
+++ CARApp/app/src/main/java/com/car/carsquad/carapp/DriverFinishedRideActivity.java
@@ -2,6 +2,7 @@ package com.car.carsquad.carapp;
 
 import android.content.Intent;
 import android.icu.text.StringSearch;
+import android.support.annotation.NonNull;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,8 +10,11 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.sql.Driver;
 import java.util.Objects;
@@ -44,12 +48,29 @@ public class DriverFinishedRideActivity extends AppCompatActivity implements Vie
     @Override
     public void onClick(View view) {
         if(view == payCard){
-            finishDB.child(postId).child(riderId).child("paidByCash").setValue("false");
-            finishDB.child(postId).child(riderId).child("rideStatus").setValue("completed");
+
+            FirebaseDatabase.getInstance().getReference().child("post").child(postId).addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    Post completedPost = dataSnapshot.getValue(Post.class);
+                    finishDB.child(riderId).child(postId).child("compPost").setValue(completedPost);
+                    finishDB.child(riderId).child(postId).child("paymentType").setValue("card");
+                }
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {}
+            });
             finish();
         } else if (view == payCash) {
-            finishDB.child(postId).child(riderId).child("paidByCash").setValue("true");
-            finishDB.child(postId).child(riderId).child("rideStatus").setValue("completed");
+            FirebaseDatabase.getInstance().getReference().child("post").child(postId).addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    Post completedPost = dataSnapshot.getValue(Post.class);
+                    finishDB.child(riderId).child(postId).child("compPost").setValue(completedPost);
+                    finishDB.child(riderId).child(postId).child("paymentType").setValue("cash");
+                }
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {}
+            });
             finish();
         } else if (view == mCancel) {
             finish();
